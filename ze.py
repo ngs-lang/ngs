2f742bce78ba7d8919b6c68edbec117718430e8e
Ilya Sher <ilya.sher@coding-knight.com>
2013-10-20 08:56:09 +0300

    API and exploring

--- ze.py
+++ ze.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
 """
 Attempt to figure out the model
 """
+
+import subprocess
 
 
 class Host(object):
@@ -29,9 +33,14 @@ class Command(object):
         self.env = None
         self.inputs = {}
         self.outputs = {}
+        self.start_time = None
+        self.end_time = None
 
     def __str__(self):
         return "<Command {0}>".format([str(a) for a in self.argv])
+
+    def popen(self, **kw_args):
+        return subprocess.Popen(self.argv, **kw_args)
 
 
 class Pipe(object):
@@ -54,6 +63,18 @@ class Job(object):
         return "<Job object at {0}, commands={1}>".format(
             hex(id(self)), [str(c) for c in self.commands])
 
+    def run(self):
+        commands_count = len(self.commands)
+        last_popen = None
+        for i, c in enumerate(self.commands):
+            kw_args = {}
+            if i < commands_count - 1:
+                kw_args['stdout'] = subprocess.PIPE
+            if i > 0:
+                kw_args['stdin'] = last_popen.stdout
+            last_popen = c.popen(**kw_args)
+        last_popen.communicate()
+
 
 class HistoryList(object):
     pass
@@ -67,8 +88,10 @@ class MenthionedHistoryList(HistoryList):
     pass
 
 
-j = Job()
-j.withCommand(Command(argv=['ps', '-ef']))
-j.withCommand(Command(argv=['grep', 'vim']))
-# TODO: connect the commands with a pipe
-print j
+if __name__ == '__main__':
+    j = Job()
+    j.withCommand(Command(argv=['ps', '-ef']))
+    j.withCommand(Command(argv=['grep', 'vim']))
+    # TODO: connect the commands with a pipe
+    print j
+    j.run()
