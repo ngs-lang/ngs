3e2fcaa07d964a5a1ab4c050fa3c1959f0365d82
Ilya Sher <ilya.sher@coding-knight.com>
2015-09-06 22:48:41 +0300

    Exploring flex, bison, etc

---
+++ c/obj.h
@@ -0,0 +1,25 @@
+// NGS objects
+#include <stdint.h>
+typedef union {
+	int32_t num;
+	void * ptr;
+} obj;
+
+// malloc() memory is 8 byte aligned
+// ....000 - int number
+// ....001 - null
+// ....010 - false
+// ....011 - true
+// ....100 - empty string
+// ....101 - 
+// ....110 - object
+
+#define META_BITS (3)
+#define META_AND  (7)
+
+#define IS_INT(obj)    (obj.num & META_AND == 0)
+// TODO: handle situation when n is wider than int32_t - META_BITS bits
+#define SET_INT(obj,n) obj.num = n << META_BITS
+#define GET_INT(n)     (obj.num >> META_BITS)
+
+#define MK_OBJ         ((obj *) malloc(sizeof(obj)))
