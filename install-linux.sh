7a24f64133ad43fd02a81e9a809db2d244b5f9fa
Ricardo Gomes <rg@beame.io>
2018-06-11 19:30:22 +0200

    Added install script to linux

---
+++ install-linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get install uthash-dev libgc-dev libffi6 libffi-dev libjson-c-dev peg libpcre3-dev make cmake pandoc pkg-config build-essential
+sudo type awk || sudo apt-get install gawk
+
+mkdir build && cd build && cmake .. && make && sudo make install
