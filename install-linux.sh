1c96625a230d4c1716305b198d86a01e8ce15246
Ricardo Gomes <ricardo@heptasoft.com>
2018-12-30 19:44:19 +0100

    Added missing -y

--- install-linux.sh
+++ install-linux.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt-get install uthash-dev libgc-dev libffi6 libffi-dev libjson-c-dev peg libpcre3-dev make cmake pandoc pkg-config build-essential
+sudo apt-get install -y uthash-dev libgc-dev libffi6 libffi-dev libjson-c-dev peg libpcre3-dev make cmake pandoc pkg-config build-essential
 sudo type awk || sudo apt-get install gawk
 
 make install
