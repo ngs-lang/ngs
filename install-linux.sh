df6488e8e9a60aef15402bf24303d7fa26165138
Ilya Sher <is+github@beame.io>
2019-04-13 16:25:32 +0300

    Revert "Update install-linux.sh"
    This reverts commit 29e66f476448e63df765e5f11a87f39e334821d3.
    
    Modifying commits should go into dev branch.
    This one can not be merged into dev because of conflict so reverting.

--- install-linux.sh
+++ install-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install uthash-dev libgc-dev libffi6 libffi-dev libjson-c-dev peg libpcre3-dev make cmake pandoc pkg-config build-essential
+sudo apt-get install uthash-dev libgc-dev libffi6 libffi-dev libjson-c-dev peg libpcre3-dev make cmake pandoc pkg-config build-essential
 sudo type awk || sudo apt-get install gawk
 
 mkdir build && cd build && cmake .. && make && sudo make install
