a92d00bdd27f49eeebf15946fbd3466eb45a7dd3
Ilya Sher <ilya.sher@coding-knight.com>
2019-10-19 20:26:37 +0300

    [small-poc] Improve UI poc

--- small-poc/server.js
+++ small-poc/server.js
@@ -1,97 +1,93 @@
-var https = require('https');
-var fs = require('fs');
-var node_static = require('node-static');
-var fileServer = new node_static.Server('./www');
+const https = require('https');
+const fs = require('fs');
+const node_static = require('node-static');
+const fileServer = new node_static.Server('./www');
 
+const PR_TIME = 8000; // seconds * 1000
 
-var options = {
+const options = {
 	key: fs.readFileSync('ssl/mysitename.key'),
 	cert: fs.readFileSync('ssl/mysitename.crt')
 };
 
-var https_server = https.createServer(options, function (req, res) {
+const server = https.createServer(options, function (req, res) {
 	fileServer.serve(req, res);
 });
 
-var io_server = require('socket.io').listen(https_server);
+const io_server = require('socket.io').listen(server);
 
-https_server.listen(8443);
+server.listen(8443);
 
-var commands = {
+const commands = {
 	'pr': function (cmd, socket) {
-		socket.emit('progress', { id: cmd.id, obj: { text: 'working' } });
-		for (var i = 0; i < 20; i++) {
-			console.log('for', i);
-			(function (i) {
-				console.log('func', i);
-				setTimeout(function () {
-					console.log('setTimeout', i);
-					socket.emit('progress', { id: cmd.id, obj: { text: 'working', pct: i * 5 } });
-				}, i * 200);
-			})(i);
+		socket.emit('progress', {id: cmd.id, obj: {text: 'working'}});
+		for (let i = 0; i < 20; i++) {
+			setTimeout(function () {
+				socket.emit('progress', {id: cmd.id, obj: {text: 'working', pct: i * 5}});
+			}, i * PR_TIME / 20);
 		}
 		setTimeout(function () {
-			socket.emit('progress', { id: cmd.id, obj: { text: 'done', pct: 100 } });
-		}, 4000);
+			socket.emit('progress', {id: cmd.id, obj: {text: 'done', pct: 100}});
+		}, PR_TIME);
 	},
 	'fail': function (cmd, socket) {
-		socket.emit('progress', { id: cmd.id, obj: { text: 'working' } });
+		socket.emit('progress', {id: cmd.id, obj: {text: 'working'}});
 		setTimeout(function () {
-			socket.emit('progress', { id: cmd.id, obj: { text: 'error' } });
+			socket.emit('progress', {id: cmd.id, obj: {text: 'error'}});
 		}, 1000);
 	},
 	'sleep': function (cmd, socket) {
-		socket.emit('progress', { id: cmd.id, obj: { text: 'working' } });
+		socket.emit('progress', {id: cmd.id, obj: {text: 'working'}});
 		setTimeout(function () {
-			socket.emit('progress', { id: cmd.id, obj: { text: 'done' } });
+			socket.emit('progress', {id: cmd.id, obj: {text: 'done'}});
 		}, 5000);
 	},
 	'ls': function (cmd, socket) {
-		socket.emit('progress', { id: cmd.id, obj: { text: 'working' } });
-		var path = '.';
+		socket.emit('progress', {id: cmd.id, obj: {text: 'working'}});
+		let path = '.';
 		if (cmd.args && cmd.args[0]) {
 			path = cmd.args[0];
 		}
 		fs.readdir(path, function (err, files) {
 			if (err) {
-				socket.emit('error', { id: cmd.id, text: err.toString() });
-				socket.emit('progress', { id: cmd.id, obj: { text: 'error' } });
+				socket.emit('error', {id: cmd.id, text: err.toString()});
+				socket.emit('progress', {id: cmd.id, obj: {text: 'error'}});
 				return;
 			}
-			var c = files.length;
+			let c = files.length;
 			files.forEach(function (file, i) {
 				fs.stat(path + '/' + file, function (err, stats) {
 					if (err) {
-						socket.emit('error', { id: cmd.id, text: 'Could not stat() ' + file });
-						socket.emit('progress', { id: cmd.id, obj: { text: 'error' } });
+						socket.emit('error', {id: cmd.id, text: 'Could not stat() ' + file});
+						socket.emit('progress', {id: cmd.id, obj: {text: 'error'}});
 						return;
 					}
-					var t = 'file';
+					let t = 'file';
 					if (stats.isDirectory()) {
 						t = 'directory';
 					}
-					socket.emit('output', { id: cmd.id, obj: { 'type': t, 'text': file, 'path': path, 'stat': stats } });
+					socket.emit('output', {id: cmd.id, obj: {'type': t, 'text': file, 'path': path, 'stat': stats}});
 					c--;
-					if (c == 0) {
-						socket.emit('progress', { id: cmd.id, obj: { text: 'done' } });
+					if (c === 0) {
+						socket.emit('progress', {id: cmd.id, obj: {text: 'done'}});
 					}
 				});
 			});
 		});
 	}
-}
+};
 
-io_server.sockets.on('connection', function (socket) {
-	console.log('Socket.IO connection', socket.handshake.address)
+io_server.on('connection', function (socket) {
+	console.log('Socket.IO connection', socket.handshake.address);
 	if (!['127.0.0.1', '::ffff:127.0.0.1'].includes(socket.handshake.address)) {
-		console.error(`Closing connection from ${socket.handshake.address}`)
+		console.error(`Closing connection from ${socket.handshake.address}`);
 		socket.disconnect();
 		return;
 	}
 	socket.on('command', function (cmd) {
 		console.log('command!', cmd);
 		if (!commands[cmd.cmd]) {
-			socket.emit('progress', { id: cmd.id, obj: { text: 'error' } });
+			socket.emit('progress', {id: cmd.id, obj: {text: 'error'}});
 			return;
 		}
 		commands[cmd.cmd](cmd, socket);
