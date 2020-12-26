9faf40599e253d375aaf9594588186d444633c79
Rui Chen <rui@meetup.com>
2020-12-25 22:36:24 -0500

    replace sed with gsed
    Signed-off-by: Rui Chen <rui@meetup.com>

--- build-scripts/make-syntax-auto.sh
+++ build-scripts/make-syntax-auto.sh
@@ -2,6 +2,23 @@
 
 set -eu
 
-sed -n '/^typedef/p; /^struct/,/^$/p; /define YY_MAX_LINES/p;'
+SED=sed
+
+# gsed - MacOS, sed - all the rest
+if which gsed >/dev/null;then
+	SED=gsed
+fi
+
+if ! $SED --version >/dev/null 2>/dev/null;then
+	echo "Failed to assert GNU sed: failed running sed --version" >&2
+	exit 1
+fi
+
+if ! $SED --version | grep -iq 'GNU sed';then
+	echo "Failed to assert GNU sed: 'sed --version' output had no 'GNU sed'" >&2
+	exit 2
+fi
+
+$SED -n '/^typedef/p; /^struct/,/^$/p; /define YY_MAX_LINES/p;'
 echo 'int yyparse(yycontext *yyctx);'
 echo 'yycontext * yyrelease(yycontext *yyctx);'
