59f4159bd663a580096e85bfa2f48fefe098e5d8
Ilya Sher <ilya.sher@coding-knight.com>
2021-05-30 23:02:30 +0300

    WIP: #422 - Create install.sh

---
+++ site/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Requires: curl, sudo
+
+set -eu
+
+echo "+ Will install Next Generation Shell"
+
+echo "+ Creating temporary directory"
+
+# mktemp template argument is a must for MacOS, do not remove
+d=$(mktemp -d ngs.XXXXXXXX)
+
+echo "  + Temporary directory is at $d"
+
+(
+	cd "$d"
+	echo "+ Downloading and extracting NGS"
+
+	curl -L https://github.com/ngs-lang/ngs/archive/master.tar.gz | tar --strip-components=1 -xzf -
+
+	echo "+ Running installer"
+	./install.sh
+)
+
+echo "+ Removing temporary directory $d"
+
+rm -rf "$d"
+
+echo "+ NGS installed successfully. Have fun!"
