f2d4e93b761ffb35ef9336b87ac08189273b27a9
Ilya Sher <34807039+ilyash-b@users.noreply.github.com>
2024-07-10 10:56:11 +0400

    [build] cleanup attempt

--- build.macos.env.sh
+++ build.macos.env.sh
@@ -1,3 +1,3 @@
-pcp=$(dirname $(brew list pkg-config | grep '/bin/pkg-config'))
-export PATH="$pcp:$PATH"
-export PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig
+# pcp=$(dirname $(brew list pkg-config | grep '/bin/pkg-config'))
+# export PATH="$pcp:$PATH"
+# export PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig
