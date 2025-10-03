6a03cb1f5a2173c6aaca48155a8d6151181cd21b
Ricardo Gomes <ricardo@heptasoft.com>
2025-10-03 12:12:52 +0100

    WIP: fixes

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ else
 				$SUDO yum install -y ctest
 		  fi
 		fi
-		$SUDO yum group install "Development Tools"
+		$SUDO dnf5 group install development-tools
 	elif type pacman &>/dev/null;then
 		echo "  + On Linux / pacman. Installing apt packages."
 		$SUDO pacman -Sy --noconfirm peg make cmake pandoc pkgconfig pcre
